40caee8b96ac131a4f69b72ff022c3c6c1c52ad3
guillemrh <guillemmm.98@gmail.com>
2022-08-08 18:38:48 +0200

    first commit

---
+++ Spotify_reviews.py
@@ -0,0 +1,11 @@
+import string
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+#%%
+df = pd.read_csv('../Spotify app reviews/reviews.csv')
+print(df.head())
+# %%

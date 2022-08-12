d2eec9ccbb37ac77dda791383bf8f3691778f704
guillemrh <guillemmm.98@gmail.com>
2022-08-12 17:21:40 +0200

    Time and date

--- Spotify_reviews.py
+++ Spotify_reviews.py
@@ -1,11 +1,73 @@
+#%%
 import string
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import tensorflow as tf
+import tensorflow_hub as hub
+from wordcloud import WordCloud
 
-#%%
+
+
 df = pd.read_csv('../Spotify app reviews/reviews.csv')
 df.head()
+df.info()
+df.describe()
+df.isna().sum()
+
+#set color for data visualization
+sns.set(rc={"axes.facecolor":"#ffffff","figure.facecolor":"#ffffff", "grid.color":"#99e2b4",
+            "axes.edgecolor":"#99e2b4", "axes.labelcolor":"#195e2d", "xtick.color":"#195e2d",
+            "ytick.color":"#195e2d"})
+
+
+palettes = ['#eee82c', '#91cb3e', '#53a548', '#4c934c', '#19381f']
+cmap = colors.ListedColormap(['#137547', '#2a9134', '#3fa34d', '#5bba6f', '#19381f'])
+
+# Data cleaning and EDA
+#%%
+reply = df[~df['Reply'].isna()]
+
+ax = sns.countplot(reply['Rating'], palette=palettes, edgecolor=palettes)
+ax.set_title('Rating of Replied Reviews')
+
+plt.figure(figsize=(10,10))
+wc = WordCloud(max_words=1000, min_font_size=10, height=600, width=1200, collocations=False,
+               background_color='#e6ffed', colormap='summer').generate(' '.join(reply['Reply']))
+
+plt.axis('off')
+plt.imshow(wc)
+# %%
+def top_words(text):
+    word = pd.DataFrame(wc.process_text(''.join(text)).items()).sort_values(1, ascending=False)
+    
+    print('Top 5 words that appear most frequently:\n')
+    for i in range(5):
+        item = word.iloc[i]
+        print(item[0] + '\t--> ' + str(item[1]))
+        
+top_words(reply['Reply'])
+# %%
+#Data relationship with time or date
+
+df['Time_submitted'] = pd.to_datetime(df['Time_submitted'])
+
+df['Date'] = df['Time_submitted'].apply(lambda x: x.date())
+
+reviews_per_day = df.groupby(df['Date']).count()['Review']
+reviews_per_day = reviews_per_day.reset_index()
+reviews_per_day['Month'] = reviews_per_day['Date'].apply(lambda x: x.month)
+reviews_per_day.rename(columns={'Review':'Total_review'}, inplace=True)
+reviews_per_day.head()
+
+ax = sns.lineplot(data=reviews_per_day, x='Date', y='Total_review', color='#4c934c')
+ax.set_title('Total Review per Day')
+
+g = sns.FacetGrid(data=reviews_per_day, col='Month', col_wrap=4)
+g.map(plt.plot, 'Total_review', color='#4c934c')
+g.set_axis_labels(x_var='Day', y_var='Total Review')
+
+ax = sns.displot(data=reviews_per_day, x='Total_review', kind='kde', fill=True, color='#4c934c')
 # %%
